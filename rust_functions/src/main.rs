9b4c8483c1dcbab727578d08864bd06e3cbda383
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-13 00:06:58 +0530

    Add sum_value function to calculate and print the sum of two numbers

--- rust_functions/src/main.rs
+++ rust_functions/src/main.rs
@@ -1,7 +1,13 @@
 fn main() {
     print_value();
+    sum_value(10, 20);
 }
 
 fn print_value() {
     println!("This is a function");
+}
+fn sum_value(num1:u8, num2:u8){
+    let item = num1 + num2;
+    println!("the sum of {} and {} is - {}",num1, num2, item);
+
 }
