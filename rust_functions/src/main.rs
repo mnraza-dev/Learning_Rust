fcd1733a3f0bead2a37625f7dc365f6b8370ea16
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-13 00:02:27 +0530

    Remove redundant comments from main.rs for clarity

--- rust_functions/src/main.rs
+++ rust_functions/src/main.rs
@@ -1,10 +1,6 @@
 fn main() {
     print_value();
 }
-// Functions
-// Functions are declared using the fn keyword
-// Functions can take parameters and return values
-// Functions can also be declared with a return type
 
 fn print_value() {
     println!("This is a function");
