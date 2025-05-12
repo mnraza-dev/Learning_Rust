333acb080547b15f522ab9674bc547985c58d5ce
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-13 03:37:06 +0530

    Refactor main function to clarify ownership transfer and update string processing

--- rust_ownership/src/main.rs
+++ rust_ownership/src/main.rs
@@ -1,24 +1,32 @@
 fn main() {
     let x: u8 = 5;
-    let s1 = String::from("hello");
-    let s2 = s1; // ownership moves to s2
+    let s1 = String::from("hello there..!");
+    // let s2 = s1; // ownership moves to s2
 
     // println!("{}", s1); // ❌ error! s1 is no longer valid
-    println!("{}", s2); // ✅ s2 is valid
+    println!("{}", s1); // ✅ s2 is valid
 
     process_interger(x);
+    process_string(s1); 
+
+    // println!("{}", s1); // ❌ error! s1 is no longer valid
 }
 
 fn process_interger(x: u8) {
     println!("{}", x);
 }
+fn process_string(s: String) {
+    println!("{}", s);
+}
 
 /*
 NOTE: Ownership
 🎯 The Three Rules of Ownership
-Each value has a single owner.
 
-A value can only have one owner at a time.
+1. Each value has a single owner.
 
-When the owner goes out of scope, the value is dropped.
+2. A value can only have one owner at a time.
+
+3. When the owner goes out of scope, the value is dropped.
+
  */
