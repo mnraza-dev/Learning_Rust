4a5d68849e3cf2f3c638f922064b2de872f548e6
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-13 02:53:37 +0530

    Refactor main function to demonstrate Rust ownership and update .gitignore to include build artifacts

--- rust_ownership/src/main.rs
+++ rust_ownership/src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("hello");
+    let s2 = s1; // ownership moves to s2
+
+    println!("{}", s1); // ❌ error! s1 is no longer valid
 }
