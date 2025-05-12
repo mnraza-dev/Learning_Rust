802433773f92cfaf61600c59bdc331b895151f7a
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-12 22:48:40 +0530

    Add .gitignore and initial Rust main file

---
+++ hello_rust/target/debug/deps/hello_rust-9057bf2d1afec3a9.d
@@ -0,0 +1,5 @@
+C:\Users\mnraza\Desktop\Learning_Rust\hello_rust\target\debug\deps\libhello_rust-9057bf2d1afec3a9.rmeta: src\main.rs
+
+C:\Users\mnraza\Desktop\Learning_Rust\hello_rust\target\debug\deps\hello_rust-9057bf2d1afec3a9.d: src\main.rs
+
+src\main.rs:
---
+++ hello_rust/target/debug/deps/hello_rust-c91ddf145e183191.d
@@ -0,0 +1,5 @@
+C:\Users\mnraza\Desktop\Learning_Rust\hello_rust\target\debug\deps\libhello_rust-c91ddf145e183191.rmeta: src\main.rs
+
+C:\Users\mnraza\Desktop\Learning_Rust\hello_rust\target\debug\deps\hello_rust-c91ddf145e183191.d: src\main.rs
+
+src\main.rs:
