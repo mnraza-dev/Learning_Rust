d4540148732f916200a3b76f446496c494fe09b5
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-12 23:51:22 +0530

    Add function demonstration and update .rustc_info.json with new fingerprint

--- hello_rust/src/main.rs
+++ hello_rust/src/main.rs
@@ -44,5 +44,15 @@ fn main() {
     let (name, age) = emp_info; 
 
     println!("The name, age of the employee is {}, {}", name, age);
-    
+
+   // Functions 
+   // Functions are declared using the fn keyword
+   // Functions can take parameters and return values
+   // Functions can also be declared with a return type
+   
+   fn print_value(){
+         println!("This is a function");
+   }
+    print_value(); 
+
 }
