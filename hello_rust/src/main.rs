c427d0edb76a3d4b683b3ee52af8120062e1c239
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-12 23:24:50 +0530

    Format variable declarations for consistency and update .rustc_info.json with new fingerprint

--- hello_rust/src/main.rs
+++ hello_rust/src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
-    let num:u8 = 56;
+    let num: u8 = 56;
 
     // num = 100; // This will cause an error because num is immutable
     // To fix this, we can declare num as mutable
 
-    let mut num:u8 = 56;
+    let mut num: u8 = 56;
     num = 100; // Now this is valid
     println!("The value stored in num is {}", num);
 
@@ -12,7 +12,29 @@ fn main() {
     // We can also use the `let` keyword to create a new variable with the same name
     // but a different type
 
-    let num:u16 = 1000; // This is valid because num is now a new variable
-
+    let num: u16 = 1000; // This is valid because num is now a new variable
     println!("The value stored in num is {}", num);
+
+
+    //  String Literals - Fixed length string
+    // String - Dynamic length string
+    // String literals are immutable and stored in the read-only memory
+    // String objects are mutable and stored in the heap
+    // String literals are created using double quotes
+    // String objects are created using the String::from() method
+
+    let string_literal: &str = "Hello, there!"; // This is a string literal
+    println!("The value stored in string_literal is {}", string_literal);
+
+    let mut string = String::from("Hello, there!"); // This is a String object
+    string.push_str(" How are you?"); // This is valid because string is mutable
+
+    println!("The value stored in string is {}", string);
+
+    // Rust has a special type called `Option` that can be used to represent a value that may or may not be present
+    let optional_value: Option<u8> = Some(56);
+    println!("The value stored in optional_value is {:?}", optional_value);
+
+    let optional_value: Option<u8> = None;
+    println!("The value stored in optional_value is {:?}", optional_value);
 }
