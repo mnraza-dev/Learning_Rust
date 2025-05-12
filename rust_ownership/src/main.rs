f0abcb0f806f2b058aa1a0eeaa908e448d722ae5
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-13 03:26:41 +0530

    Refactor main function to demonstrate ownership transfer and add integer processing

--- rust_ownership/src/main.rs
+++ rust_ownership/src/main.rs
@@ -1,6 +1,14 @@
 fn main() {
+    let x: u8 = 5;
     let s1 = String::from("hello");
     let s2 = s1; // ownership moves to s2
 
-    println!("{}", s1); // ❌ error! s1 is no longer valid
+    // println!("{}", s1); // ❌ error! s1 is no longer valid
+    println!("{}", s2); // ✅ s2 is valid
+
+    process_interger(x);
+}
+
+fn process_interger(x: u8) {
+    println!("{}", x);
 }
