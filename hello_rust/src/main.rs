8078a3cade136d359201d906fcf535107302937e
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-12 23:11:30 +0530

    Refactor main function to demonstrate mutable variable usage and update .rustc_info.json

--- hello_rust/src/main.rs
+++ hello_rust/src/main.rs
@@ -1,5 +1,18 @@
 fn main() {
-    let num: u8 = 15;
+    let num:u8 = 56;
+
+    // num = 100; // This will cause an error because num is immutable
+    // To fix this, we can declare num as mutable
+
+    let mut num:u8 = 56;
+    num = 100; // Now this is valid
+    println!("The value stored in num is {}", num);
+
+    // Rust does not allow you to change the type of a variable
+    // We can also use the `let` keyword to create a new variable with the same name
+    // but a different type
+
+    let num:u16 = 1000; // This is valid because num is now a new variable
 
     println!("The value stored in num is {}", num);
 }
